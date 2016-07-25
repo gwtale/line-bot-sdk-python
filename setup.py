d3621a8d3acb1e0b168c2aaca0709a0429570ad5
studio3104 <studio3104.com@gmail.com>
2016-07-25 17:06:35 +0900

    Bumpup version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requires = [str(r.req) for r in parse_requirements(requirements_txt, session=Fal
 
 setup(
     name='linebot',
-    version='0.3.1',
+    version='0.3.2',
     packages=['linebot'],
     install_requires=requires,
     author='Satoshi SUZUKI',
